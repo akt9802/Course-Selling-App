01e8f495ca89e13e6c08b7c330d84f506c58482b
Aman Kumar <akt9802@gmail.com>
2025-06-04 18:41:23 +0530

    just initializing the project

---
+++ index.js
@@ -0,0 +1,24 @@
+// this is a course selling app
+
+const express = require('express');
+const app = express();
+
+app.post('/signup',(req,res) => {
+
+})
+
+app.post('/signin',(req,res) => {
+
+})
+
+app.post('/createcourse',(req,res)=> {
+
+})
+
+app.post('/seecourse',(req,res) => {
+
+})
+
+app.listen(3000,() => {
+    console.log("App is running at PORT : 3000");
+})
